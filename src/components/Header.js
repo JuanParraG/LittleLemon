f4e6135d2b66b2f32e40262366f082254cc74e2d
Your Name <you@example.com>
2024-01-02 21:02:35 -0500

    Configuracion de doc html

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import logo from "../sources/Logo.svg"
+
+function Header(){
+    return(
+        <header>
+             <img src={logo} alt="Yellow Lemon. Little lemon's logo."></img>
+        </header>
+    )
+}
+export default Header;
